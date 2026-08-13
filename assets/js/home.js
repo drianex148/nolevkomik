49e5872695ae68d6c2e0eb0fa72b790d09d9c12c
drianex148 <drianaex148@gmail.com>
2026-08-13 15:35:39 +0700

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,30 +1,37 @@
-const komik = JSON.parse(localStorage.getItem("komik")) || [];
+const komik = [
+  {
+    judul: "Komik Pertamaku",
+    gambar: "gambar/onee-san-mode.jpg",
+    chapter: "Chapter 1",
+    link: "detail.html"
+  }
+];
 
-function tampilkan(data) {
-    const grid = document.getElementById("grid");
-    grid.innerHTML = "";
+function tampilkanKomik(data) {
+  const grid = document.getElementById("grid");
+  grid.innerHTML = "";
 
-    data.forEach(item => {
-        grid.innerHTML += `
-        <a href="${item.link}">
-            <div class="card" data-title="${item.judul}">
-                <img src="${item.gambar}">
-                <h3>${item.judul}</h3>
-                <p>${item.chapter}</p>
-            </div>
-        </a>
-        `;
-    });
+  data.forEach(item => {
+    grid.innerHTML += `
+      <a href="${item.link}">
+        <div class="card" data-title="${item.judul}">
+          <img src="${item.gambar}">
+          <h3>${item.judul}</h3>
+          <p>${item.chapter}</p>
+        </div>
+      </a>
+    `;
+  });
 }
 
 function cariKomik() {
-    const kata = document.getElementById("search").value.toLowerCase();
+  const keyword = document.getElementById("search").value.toLowerCase();
 
-    const hasil = komik.filter(item =>
-        item.judul.toLowerCase().includes(kata)
-    );
+  const hasil = komik.filter(item =>
+    item.judul.toLowerCase().includes(keyword)
+  );
 
-    tampilkan(hasil);
+  tampilkanKomik(hasil);
 }
 
-tampilkan(komik);
+tampilkanKomik(komik);
