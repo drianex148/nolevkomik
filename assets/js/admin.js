2e33e00ed101ec72a8adffa3b8777b0202da4eb8
drianex148 <drianaex148@gmail.com>
2026-08-13 13:12:31 +0700

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,17 @@
-function simpan(){
+function simpan() {
+    const komik = {
+        judul: document.getElementById("judul").value,
+        cover: document.getElementById("cover").value,
+        genre: document.getElementById("genre").value,
+        status: document.getElementById("status").value,
+        deskripsi: document.getElementById("deskripsi").value
+    };
 
-alert("Admin berhasil dibuat.\n\nNanti kita sambungkan ke daftar komik.");
+    let daftar = JSON.parse(localStorage.getItem("komik")) || [];
 
+    daftar.push(komik);
+
+    localStorage.setItem("komik", JSON.stringify(daftar));
+
+    alert("Komik berhasil disimpan!");
 }
