62a70a49611901014d054e2e8af9b98c0f9ae857
drianex148 <drianaex148@gmail.com>
2026-08-13 15:30:31 +0700

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,30 @@
+const komik = JSON.parse(localStorage.getItem("komik")) || [];
 
+function tampilkan(data) {
+    const grid = document.getElementById("grid");
+    grid.innerHTML = "";
+
+    data.forEach(item => {
+        grid.innerHTML += `
+        <a href="${item.link}">
+            <div class="card" data-title="${item.judul}">
+                <img src="${item.gambar}">
+                <h3>${item.judul}</h3>
+                <p>${item.chapter}</p>
+            </div>
+        </a>
+        `;
+    });
+}
+
+function cariKomik() {
+    const kata = document.getElementById("search").value.toLowerCase();
+
+    const hasil = komik.filter(item =>
+        item.judul.toLowerCase().includes(kata)
+    );
+
+    tampilkan(hasil);
+}
+
+tampilkan(komik);
